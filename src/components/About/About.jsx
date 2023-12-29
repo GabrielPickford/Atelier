d244151b3f8841f00fe2f40624a3cbad121a48ea
unknown <josegabriel.jgtc42@gmail.com>
2023-12-29 13:18:16 -0400

    header & about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,41 @@ import React from 'react';
 import './About.css';
 
 const About = () => (
-  <div>
-    About
+  <div className="div">
+  <div className="div-2">HANDMADE JEWELRY</div>
+  <div className="div-3">
+    <div className="div-4">
+      <div className="column">
+        <div className="div-5">
+          <div className="div-6">Atelier gifts</div>
+          <div className="div-7">
+            Atelier holiday gifts have inspired love since 1837. Discover
+            our most coveted designs for this season and beyond.
+          </div>
+        </div>
+      </div>
+      <div className="column-2">
+        <div className="div-8">
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/6113eae8304f764dc9607ca62a0784b0e3f951bd1be2ae224855130234a0f1ef?apiKey=98e930f8e11a4ca795d74fd9501a4b81&"
+            className="img"
+          />
+          <div className="div-9">Comprar</div>
+        </div>
+      </div>
+      <div className="column-3">
+        <div className="div-10">
+          <div className="div-11">
+            Atelier holiday gifts have inspired love since 1837. Discover
+            our most coveted designs for this season and beyond.
+          </div>
+          <div className="div-12">Ver catálogo</div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 );
 
 export default About;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,29 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="container-header">
+  <div className="borde-header">
+    <div className="div-img">
+      <div className="column-img">
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/8e71aba78cc39dbbbe44f0e82766067759916ebd78eb8f5920b0f6e193dc0ff6?apiKey=98e930f8e11a4ca795d74fd9501a4b81&"
+          className="model-girl"
+        />
+      </div>
+      <div className="column-text">
+        <div className="gift-text">
+          <div className="atelier-gifts">Atelier gifts</div>
+          <div className="holiday-text">
+            Atelier holiday gifts have inspired love since 1837. Discover
+            our most coveted designs for this season and beyond.
+          </div>
+          <div className="ver-catalogo">Ver catálogo</div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 );
 
 export default Header;
